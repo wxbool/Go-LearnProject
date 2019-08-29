68df771d24f0d88b2d74df0cb31063d9947dcedb
641453620 <641453620@qq.com>
2019-08-29 16:26:30 +0800

    鼠标点击器：优化异常捕获

--- win.control.simulate/clicks/clicks.go
+++ win.control.simulate/clicks/clicks.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	appclick "github.com.wxbool/go.learn.project/win.control.simulate/clicks/app"
+	"log"
 	"time"
 )
 
@@ -11,6 +12,15 @@ const CONFIG = "click.ini"
 
 //模拟点击小程序
 func main() {
+
+	//致命错误捕获
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("Error : %v", err)
+
+			time.Sleep(time.Second * 8)
+		}
+	}()
 
 	app := appclick.NewApp(CONFIG)
 
